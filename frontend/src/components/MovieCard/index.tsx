40fcb3cf471f60323767e9638aade90c3c551e38
Matheus Colin <mattxnice@gmail.com>
2022-09-18 00:30:47 -0300

    props (fixed the error maybe)

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -6,21 +6,24 @@ type Props = {
     movie: Movie;
 }
 
-function MovieCard(movie: Props) {
-
+function MovieCard({ movie }: Props) {
     return (
-        <div>
-            <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
-            <div className="dsmovie-card-bottom-container">
-                <h3>{movie.title}</h3>
-                <MovieScore />
-
-                <Link to={`/form/${movie.id}`}>
-                    <div className="btn btn-primary dsmovie-btn">Avaliar</div>
-                </Link>
-            </div>
+      <div>
+        <img
+          className="dsmovie-movie-card-image"
+          src={movie.image}
+          alt={movie.title}
+        />
+        <div className="dsmovie-card-bottom-container">
+          <h3>{movie.title}</h3>
+          <MovieScore />
+  
+          <Link to={`/form/${movie.id}`}>
+            <div className="btn btn-primary dsmovie-btn">Avaliar</div>
+          </Link>
         </div>
+      </div>
     );
-}
+  }
 
 export default MovieCard;
